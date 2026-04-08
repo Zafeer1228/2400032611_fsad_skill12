07b6297033c6d34cc73240fc47679df8e151acf0
K ZAFEER <2400032611@kluniversity.in>
2026-04-08 19:28:39 +0530

    Delete 2400032031-fsad-skill-12 directory

--- 2400032031-fsad-skill-12/src/main/java/com/skill/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.skill.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.skill.model.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
